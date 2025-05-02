d65570f1a7ea70fb05bac1f4ce60a664449f2aa4
Samantha Kotlicky <samanthakotlicky@Samanthas-MacBook-Pro-2.local>
2025-05-02 16:29:33 -0500

    Add CORS fix

--- pages/api/articles.js
+++ pages/api/articles.js
@@ -1,15 +1,11 @@
 export default async function handler(req, res) {
-    // Handle preflight requests
-    if (req.method === 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      res.status(200).end();
-      return;
-    }
-  
-    // Set CORS header for all other requests
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end(); // handle preflight requests
+    }
   
     const { sectionId } = req.query;
   
